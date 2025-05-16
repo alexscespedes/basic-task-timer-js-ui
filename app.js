37e083cab681f11d7bfb1f858f6d7f6f5f89f09a
alexscespedes <alexandersenc99@gmail.com>
2025-05-16 04:51:51 -0400

    add js addTask in the html

---
+++ app.js
@@ -0,0 +1,21 @@
+// Get Dom Elements
+
+const taskName = document.getElementById("taskName");
+const taskDescription = document.getElementById("taskDescription");
+const taskList = document.getElementById("taskList");
+
+let tasks = [];
+
+// Check my code with taskManager.js
+
+function addTask() {
+  const name = taskName.value.trim();
+  const description = taskDescription.value.trim();
+
+  if (name === "") {
+    alert("Please enter a name");
+    return;
+  }
+
+  console.log(name, description);
+}
